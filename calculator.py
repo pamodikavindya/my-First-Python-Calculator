d9fcba02c31bbe73731c0348f3453c9b81b8fa15
pamodikavindya <pamodikavindya0@gmail.com>
2026-04-11 01:46:54 +0900

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,27 @@
+# Simple Calculator Project
+print("1. Addition (+)")
+print("2. Subtraction (-)")
+print("3. Multiplication (*)")
+print("4. Division (/)")
+
+choice = input("Enter operation number (1/2/3/4): ")
+
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if choice == '1':
+    print(f"Result: {num1 + num2}")
+elif choice == '2':
+    print(f"Result: {num1 - num2}")
+elif choice == '3':
+    print(f"Result: {num1 * num2}")
+elif choice == '4':
+    if num2 != 0:
+        print(f"Result: {num1 / num2}")
+    else:
+        print("Error: Cannot divide by zero")
+else:
+    print("Invalid Input")
+
+input("\nExit wenna Enter obanna...")
